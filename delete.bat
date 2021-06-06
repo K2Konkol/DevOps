2d95217d8aa40b76ea6cefc5270779ed351ad736
Krzysztof Konkol <kkonkol@sii.pl>
2021-06-06 18:27:41 +0200

    K8s deployment

---
+++ delete.bat
@@ -0,0 +1,16 @@
+
+kubectl delete deploy myapi                
+kubectl delete deploy mybroker             
+kubectl delete deploy myimageuploader      
+kubectl delete deploy mynginx              
+kubectl delete deploy mypostgres-deployment
+kubectl delete deploy myredis              
+kubectl delete deploy myweb                
+kubectl delete deploy myworker             
+kubectl delete svc myapi          
+kubectl delete svc mybroker       
+kubectl delete svc myimageuploader
+kubectl delete svc mynginx        
+kubectl delete svc mypg           
+kubectl delete svc myredis        
+kubectl delete svc myweb          
